b77b131e19fb7776399a4f1bd32a2956c804e870
als8921 <als8921@naver.com>
2024-12-27 09:38:59 +0900

    feat : store Model 추가

---
+++ back_end/store/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Store
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'number', 'address')  # 리스트에 표시할 필드
+    search_fields = ('name', 'address')  # 검색 가능 필드
+
+# 또는 아래와 같이 간단하게 등록할 수도 있습니다.
+# admin.site.register(Store)
