2b2b2713faa90fee624f94bc63033322c8d2d8cc
wnstjd9288 <wnstjd9288@naver.com>
2025-01-12 19:52:43 +0900

    add : store_detail에 종속되는 3가지 페이지 추가

---
+++ front_end/lib/screen/store/screen_store_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoreMenuScreen extends StatefulWidget {
+  const StoreMenuScreen({super.key});
+
+  @override
+  State<StoreMenuScreen> createState() => _StoreMenuScreenState();
+}
+
+class _StoreMenuScreenState extends State<StoreMenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ front_end/lib/screen/store/screen_store_review.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoreReviewScreen extends StatefulWidget {
+  const StoreReviewScreen({super.key});
+
+  @override
+  State<StoreReviewScreen> createState() => _StoreReviewScreenState();
+}
+
+class _StoreReviewScreenState extends State<StoreReviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
