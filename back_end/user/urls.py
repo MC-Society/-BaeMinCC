dae6ab9a22f9cdeb07a5d4c07c40eaf891597de8
wnstjd9288 <wnstjd9288@naver.com>
2025-01-19 17:53:02 +0900

    test : 테스트

--- back_end/user/urls.py
+++ back_end/user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, Authorize
+from .views import UserViewSet
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet)
 
 urlpatterns = [
-    path('user/auth/', Authorize, name = "Authorize"),
+    path('user/auth/', UserViewSet.isLoginValid),
     path('', include(router.urls)),
 ]
--- back_end/user/views.py
+++ back_end/user/views.py
@@ -8,19 +8,18 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-@api_view(["POST"])
-def Authorize(request):
+    def isLoginValid(self, request, *args, **kwargs):
+        user_id = request.data.get('UserID')
+        password = request.data.get('pw')
 
-    user_id = request.data.get('UserID')
-    password = request.data.get('pw')
+        try:
+            user = User.objects.get(UserID=user_id)
 
-    try:
-        user = User.objects.get(UserID=user_id)
+            if user.pw == password:
+                return Response({"id": user.id}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 
-        if user.pw == password:
-            return Response({"id": user.id}, status=status.HTTP_200_OK)
-        else:
-            return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-
-    except User.DoesNotExist:
-        return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        except User.DoesNotExist:
+            return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        
