f8f1faf9aba677e5814efc2fd52634cc0d604294
als8921 <als8921@naver.com>
2025-01-19 17:17:19 +0900

    feat : 유저 인증 부분 ViewSet에서 빼기, 인증 완료시 유저의 id Response

--- back_end/user/views.py
+++ back_end/user/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import api_view
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import User
@@ -7,17 +8,19 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def create(self, request, *args, **kwargs):
-        user_id = request.data.get('UserID')
-        password = request.data.get('pw')
+@api_view(["POST"])
+def Authorize(request):
 
-        try:
-            user = User.objects.get(UserID=user_id)
+    user_id = request.data.get('UserID')
+    password = request.data.get('pw')
 
-            if user.pw == password:
-                return Response({"message": "Authentication successful"}, status=status.HTTP_200_OK)
-            else:
-                return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+    try:
+        user = User.objects.get(UserID=user_id)
 
-        except User.DoesNotExist:
-            return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        if user.pw == password:
+            return Response({"id": user.id}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    except User.DoesNotExist:
+        return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
