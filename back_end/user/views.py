9aebc21e2450a18f38ab6949f88c0df3d0cc02e1
als8921 <als8921@naver.com>
2025-01-19 16:45:58 +0900

    feat : 유저의 id, pw가 맞는지 확인

--- back_end/user/views.py
+++ back_end/user/views.py
@@ -1,7 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_id = request.data.get('UserID')
+        password = request.data.get('pw')
+
+        try:
+            user = User.objects.get(UserID=user_id)
+
+            if user.pw == password:
+                return Response({"message": "Authentication successful"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        except User.DoesNotExist:
+            return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
